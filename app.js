a380bf42913a30622c5768c1c4040b7e699e5dc2
brennoaf <131369370+brennoaf@users.noreply.github.com>
2024-10-28 21:06:22 -0300

    function to remove repetitions created

--- app.js
+++ app.js
@@ -19,25 +19,44 @@ app.use(cors({
     },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     optionsSuccessStatus: 200,
-  }));
+}));
 
 //definir cabeçalho
 app.use((req, res, next) => {
-const origin = req.get('origin');
-if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-}
-next();
+    const origin = req.get('origin');
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    next();
 });
 
 //processar erros com middleware
 app.use((err, req, res, next) => {
-console.error('Erro detectado:', err.message);
-res.status(500).json({ error: 'Erro no servidor' });
+    console.error('Erro detectado:', err.message);
+    res.status(500).json({ error: 'Erro no servidor' });
 });
   
 
 const data_local = './data/'
+
+function parseData(filePath, res) {
+
+    fs.readFile(filePath, 'utf8', (err, csvData) => {
+        if (err) {
+        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
+        }
+    
+        Papa.parse(csvData, {
+        header: true,
+        complete: function (results) {
+            res.json(results.data); 
+        },
+        error: function (error) {
+            res.status(500).json({ error: 'Erro ao processar o CSV' });
+        },
+        });
+    });
+}
 
 
 //aeroporto
@@ -46,63 +65,21 @@ const data_local = './data/'
 app.get('/api/data/aeroporto/2024', (req, res) => {
   const filePath = path.join(data_local, 'aeroporto/2024/resumo_anual.csv');
 
-  fs.readFile(filePath, 'utf8', (err, csvData) => {
-    if (err) {
-      return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-    }
-
-    Papa.parse(csvData, {
-      header: true,
-      complete: function (results) {
-        res.json(results.data); 
-      },
-      error: function (error) {
-        res.status(500).json({ error: 'Erro ao processar o CSV' });
-      },
-    });
-  });
+  parseData(filePath, res);
 });
 
 // DADOS COMPLETOS
 app.get('/api/data/aeroporto/2023', (req, res) => {
     const filePath = path.join(data_local, 'aeroporto/2023/resumo_anual.csv');
 
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 // DADOS TRATADOS
 app.get('/api/data/aeroporto/embarque-desembarque/2023_2024', (req, res) => {
     const filePath = path.join(data_local, 'aeroporto/embarque_desembarque/2023-2024/embarque_desembarque.csv');
 
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 
@@ -114,63 +91,21 @@ app.get('/api/data/aeroporto/embarque-desembarque/2023_2024', (req, res) => {
 app.get('/api/data/balanco-comercial/exportacao/2024', (req, res) => {
 const filePath = path.join(data_local, 'balanco_comercial/exportacao/2024/exportacao_municipio.csv');
 
-fs.readFile(filePath, 'utf8', (err, csvData) => {
-    if (err) {
-    return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-    }
-
-    Papa.parse(csvData, {
-    header: true,
-    complete: function (results) {
-        res.json(results.data); 
-    },
-    error: function (error) {
-        res.status(500).json({ error: 'Erro ao processar o CSV' });
-    },
-    });
-});
+    parseData(filePath, res);
 });
 
 // DADOS COMPLETOS
 app.get('/api/data/balanco-comercial/geral/2023_2024', (req, res) => {
     const filePath = path.join(data_local, 'balanco_comercial/geral/2023-2024/base_dados.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 // DADOS COMPLETOS
 app.get('/api/data/balanco_comercial/importacao/2024', (req, res) => {
     const filePath = path.join(data_local, 'balanco_comercial/importacao/2024/importacao_municipios.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 
@@ -180,21 +115,7 @@ app.get('/api/data/balanco_comercial/importacao/2024', (req, res) => {
 app.get('/api/data/emprego/empregados/2021_2024', (req, res) => {
     const filePath = path.join(data_local, 'emprego/empregados/2021-2024/empregados.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 
@@ -205,82 +126,26 @@ app.get('/api/data/emprego/empregados/2021_2024', (req, res) => {
 app.get('/api/data/empresas/por-municipio/dados-totais/2023_2024', (req, res) => {
     const filePath = path.join(data_local, 'empresas/por_municipio/dados_totais/2023-2024/empresas.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 // DADOS COMPLETOS
 app.get('/api/data/empresas/por-municipio/somente-com-baixas/2021_2024', (req, res) => {
     const filePath = path.join(data_local, 'empresas/por_municipio/somente_com_baixas/2021-2024/empresas.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 app.get('/api/data/empresas/recife/ativas/2020_2024', (req, res) => {
     const filePath = path.join(data_local, 'empresas/recife/ativas/2020-2024/empresas.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 app.get('/api/data/empresas/recife/inativas/2020_2024', (req, res) => {
     const filePath = path.join(data_local, 'empresas/recife/inativas/2020-2024/empresas.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 
@@ -289,61 +154,19 @@ app.get('/api/data/empresas/recife/inativas/2020_2024', (req, res) => {
 app.get('/api/data/ipca/indice-geral/2023_2024', (req, res) => {
     const filePath = path.join(data_local, 'ipca/indice_geral/2023-2024/indice_geral.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 app.get('/api/data/ipca/grupos/2023_2024', (req, res) => {
     const filePath = path.join(data_local, 'ipca/grupos/2023-2024/grupos.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 app.get('/api/data/ipca/tabelas/2023_2024', (req, res) => {
     const filePath = path.join(data_local, 'ipca/tabelas/2023-2024/tabelas.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 
@@ -352,21 +175,7 @@ app.get('/api/data/ipca/tabelas/2023_2024', (req, res) => {
 app.get('/api/data/pib/municipios/2010_2021', (req, res) => {
     const filePath = path.join(data_local, 'pib/municipios/2010-2021/pib_municipios.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 
@@ -375,21 +184,7 @@ app.get('/api/data/pib/municipios/2010_2021', (req, res) => {
 app.get('/api/data/pib/municipios/2010_2021', (req, res) => {
     const filePath = path.join(data_local, 'pib/municipios/2010-2021/pib_municipios.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 
@@ -398,81 +193,25 @@ app.get('/api/data/pib/municipios/2010_2021', (req, res) => {
 app.get('/api/data/ranking/2021-2024/pilares', (req, res) => {
     const filePath = path.join(data_local, 'ranking/2021-2024/ranking_pilares.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 app.get('/api/data/ranking/2021-2024/geral', (req, res) => {
     const filePath = path.join(data_local, 'ranking/2021-2024/ranking_geral.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 app.get('/api/data/ranking/2021-2024/indicador', (req, res) => {
     const filePath = path.join(data_local, 'ranking/2021-2024/ranking_indicador.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 app.get('/api/data/ranking/2021-2024/dimensao', (req, res) => {
     const filePath = path.join(data_local, 'ranking/2021-2024/ranking_dimensao.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 
@@ -481,21 +220,7 @@ app.get('/api/data/ranking/2021-2024/dimensao', (req, res) => {
 app.get('/api/data/selic/2021_2024', (req, res) => {
     const filePath = path.join(data_local, 'selic/2021-2024/taxa_selic.csv');
     
-    fs.readFile(filePath, 'utf8', (err, csvData) => {
-        if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo CSV' });
-        }
-    
-        Papa.parse(csvData, {
-        header: true,
-        complete: function (results) {
-            res.json(results.data); 
-        },
-        error: function (error) {
-            res.status(500).json({ error: 'Erro ao processar o CSV' });
-        },
-        });
-    });
+    parseData(filePath, res);
 });
 
 
